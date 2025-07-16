db79fa31b42f69f37ab00b6147572a3e5fad4ae6
Rusty <219724427+rustyshackleforddoteth@users.noreply.github.com>
2025-07-16 14:59:34 -0700

    rename to avoid collisions

--- src/core/swaps/index.ts
+++ src/core/swaps/index.ts
@@ -1,2 +1,10 @@
-export { SwapService, type SwapData, type MarketMetrics } from "./SwapService";
-export { SwapOrchestrator, type SwapUpdateParams, type EntityUpdaters } from "./SwapOrchestrator";
+export {
+  SwapService,
+  type SwapData,
+  type MarketMetrics as SwapMarketMetrics,
+} from "./SwapService";
+export {
+  SwapOrchestrator,
+  type SwapUpdateParams,
+  type EntityUpdaters,
+} from "./SwapOrchestrator";
