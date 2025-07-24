f1896ce05ca15b72cd17907ba99a312128359ed8
Rusty <219724427+rustyshackleforddoteth@users.noreply.github.com>
2025-07-24 10:32:15 -0700

    fix index file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-// Entry point for the indexer
+import { db } from "ponder:api";
+import schema from "ponder:schema";
+import { Hono } from "hono";
+import { client, graphql } from "ponder";
+
+const app = new Hono();
+
+app.use("/sql/*", client({ db, schema }));
+
+app.use("/", graphql({ db, schema }));
+app.use("/graphql", graphql({ db, schema }));
+
+export default app;
