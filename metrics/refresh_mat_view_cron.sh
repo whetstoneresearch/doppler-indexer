604f29aef22166b15067f54b2e8c51001eeabd9a
RhizoNymph <quantnymph@gmail.com>
2025-10-08 12:55:18 -0700

    Update refresh_mat_view_cron.sh

--- metrics/refresh_mat_view_cron.sh
+++ metrics/refresh_mat_view_cron.sh
@@ -1,4 +1,5 @@
 # Run this from inside metrics directory
 # Make sure postgres connection envs are set
 # Make sure POSTGRES_SCHEMA is set to current deployment
+# Change 15 to how many minutes you want to have between refreshes
 (crontab -l 2>/dev/null; echo "*/15 * * * * node 3_refresh_mat_view.js") | crontab -
