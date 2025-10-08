502bc402a16bf191d7655e5a82d8b65172fed70b
RhizoNymph <quantnymph@gmail.com>
2025-10-08 12:53:12 -0700

    Update refresh_mat_view_cron.sh

--- metrics/refresh_mat_view_cron.sh
+++ metrics/refresh_mat_view_cron.sh
@@ -1,2 +1,4 @@
 # Run this from inside metrics directory
+# Make sure postgres connection envs are set
+# Make sure POSTGRES_SCHEMA is set to current deployment
 (crontab -l 2>/dev/null; echo "*/15 * * * * node 3_refresh_mat_view.js") | crontab -
